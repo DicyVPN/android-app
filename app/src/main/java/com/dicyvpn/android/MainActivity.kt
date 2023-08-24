e4d19b43c387d5ae6082e19987803004edff0db4
Lorenzo Lapucci <halexercraft@gmail.com>
2023-08-24 16:31:20 +0200

    Add dark theme preview

--- app/src/main/java/com/dicyvpn/android/MainActivity.kt
+++ app/src/main/java/com/dicyvpn/android/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.dicyvpn.android
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dicyvpn.android.ui.theme.DicyVPNTheme
@@ -28,13 +35,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    var value by remember { mutableStateOf(false) }
+    Column {
+        Text(
             text = "Hello $name!",
             modifier = modifier
-    )
+        )
+        Button(onClick = { value = !value }) {
+            Text(text = "Click me")
+        }
+        Text(
+            text = "Value is $value",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true, name = "Dark", uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun GreetingPreview() {
     DicyVPNTheme {
