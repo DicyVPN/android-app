8204df1ff9d1fa8f615ae816606ce2ee5b2b9fdf
Lorenzo Lapucci <halexercraft@gmail.com>
2023-08-28 21:54:14 +0200

    General layout for the home activity

--- app/src/main/java/com/dicyvpn/android/MainActivity.kt
+++ app/src/main/java/com/dicyvpn/android/MainActivity.kt
@@ -1,22 +1,43 @@
 package com.dicyvpn.android
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dicyvpn.android.ui.theme.BrightGreen
 import com.dicyvpn.android.ui.theme.DicyVPNTheme
+import com.dicyvpn.android.ui.theme.Gray600
+import com.dicyvpn.android.ui.theme.Gray800
+import com.dicyvpn.android.ui.theme.Shapes
+import com.dicyvpn.android.ui.theme.Typography
 import com.dicyvpn.android.ui.theme.components.Button
 import com.dicyvpn.android.ui.theme.components.ButtonColor
 import com.dicyvpn.android.ui.theme.components.ButtonSize
@@ -29,7 +50,7 @@ class MainActivity : ComponentActivity() {
             DicyVPNTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Home()
                 }
             }
         }
@@ -37,28 +58,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var value by remember { mutableStateOf(false) }
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Button(onClick = { value = !value }, theme = ButtonTheme.LIGHT, color = ButtonColor.GREEN, size = ButtonSize.BIG) {
-            Text(text = "Click me")
+fun Home(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .padding(top = 8.dp)
+            .fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Surface(
+            modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp), color = Gray600, contentColor = Color.White, shape = Shapes.medium, shadowElevation = 8.dp
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.full_logo),
+                contentDescription = stringResource(R.string.dicyvpn_logo),
+                modifier = Modifier
+                    .padding(16.dp, 12.dp)
+                    .heightIn(max = 40.dp)
+            )
         }
-        Text(
-            text = "Value is $value",
-            modifier = modifier
-        )
+        Surface(
+            modifier
+                .fillMaxWidth()
+                .weight(1f), color = Gray800
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.world_map),
+                contentDescription = stringResource(R.string.world_map),
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .scale(1.5f)
+            )
+        }
+        Surface(modifier.fillMaxWidth(), color = Gray800) {
+            Column(modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                Text(style = Typography.bodyMedium, text = "Connesso")
+                Surface(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .height(1.dp), color = BrightGreen
+                ) {}
+                Row(modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    Text("Germania")
+                    Spacer(modifier = modifier.weight(1f))
+                    Text(fontFamily = FontFamily.Monospace, text = "DE_01")
+                    Image(painterResource(id = R.drawable.flag_de), modifier = modifier.width(24.dp).clip(Shapes.small), contentDescription = null)
+                }
+                Button({}, ButtonTheme.DARK, ButtonColor.RED, ButtonSize.NORMAL, modifier.fillMaxWidth()) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = null
+                        )
+                        Text("Disconnetti")
+                    }
+                }
+            }
+        }
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(showBackground = true, showSystemUi = true, name = "Dark", uiMode = UI_MODE_NIGHT_YES)
+@Preview(showSystemUi = true)
+@Preview(name = "TV", device = "id:tv_1080p")
 @Composable
 fun GreetingPreview() {
     DicyVPNTheme {
-        Greeting("Android")
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            Home()
+        }
     }
 }
