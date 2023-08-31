26f96d31798c45bfb6ba5e7cb1a9adf95ec4acb6
Lorenzo Lapucci <halexercraft@gmail.com>
2023-08-31 11:55:07 +0200

    Automatically logout when the refresh token is null

--- app/src/main/java/com/dicyvpn/android/MainActivity.kt
+++ app/src/main/java/com/dicyvpn/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dicyvpn.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import androidx.datastore.preferences.core.stringPreferencesKey
@@ -27,6 +29,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             DicyVPNTheme {
                 val navController = rememberNavController()
+
+                LaunchedEffect(Unit) {
+                    DicyVPN.getPreferencesDataStore().data.collect {
+                        Log.i("DicyVPN/MainActivity", "data.collect - " + it[stringPreferencesKey("auth.refreshToken")])
+                        if (it[stringPreferencesKey("auth.refreshToken")].isNullOrEmpty()) {
+                            navController.navigate("login") {
+                                popUpTo(0)
+                            }
+                        }
+                    }
+                }
+
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
